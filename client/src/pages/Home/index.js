e4b0967d9856a8151bcb5f7cd794a1d6e529a8e2
sjaiswa1 <sjaiswa1@uncc.edu>
2019-10-21 19:42:05 -0400

    Improved home page with styling

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -6,20 +6,37 @@ import background from '../../assests/background.jpg';
 
 const Styles = styled.div`
  .jumbo{
-     background: url(${background}) no-repeat fixed bottom;
-    //  background-color: aquamarine;
-    color:white;
- }
+    // background: url(${background}) no-repeat fixed bottom;
+    background-color: aquamarine;
+    color:black;
+    margin-top: 0px;
+    margin-bottom: 2px;
+    max-width: 80%;
+    margin-left: 12%;
+ };
+ .container{
+    background: url(${background}) no-repeat fixed bottom;
+    max-width: inherit;
+ };
 `;
+
+// Temporary class for generating events
+const tempforevents = [];
+let i =0;
+for(i = 0; i< 5; i++){
+    tempforevents.push(`Event number ${i}`);
+};
 
 const Home = () => {
     return(
         <Styles>
-            <Container>
-                <Jumbotron className="jumbo">
-                    Hello
-                </Jumbotron>
-            </Container>
+            <div className='container'>
+                {tempforevents.map(event=>(
+                    <Jumbotron className="jumbo">
+                        {event}
+                    </Jumbotron>
+                ))};
+            </div>
         </Styles>
     )
 }
