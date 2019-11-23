3f7bbb8e7b8b683cd4b90a5a641937e2defed129
AnushreeSrivastava <anu1106.shree@gmail.com>
2019-11-23 18:37:32 -0500

    few changes

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import styled from "styled-components";
