afdefee1f4fa7558e4b294b38aaf956b1672584d
AnushreeSrivastava <anu1106.shree@gmail.com>
2019-11-23 19:10:51 -0500

    Update test.py

--- server/src/test.py
+++ server/src/test.py
@@ -2,10 +2,17 @@
 import unittest
 from flask import Flask
 from index import app
+from unittest.mock import patch
 import json
+from Seat_Geek_API import Seat_Geek_Api
+from Database_Layer.dbController import DBController
+from unittest.mock import MagicMock
 
 BASE_URL = "http://127.0.0.1:5000"
-eventId = "5097856"  # Hardcoded eventID to test the event page
+eventId = {"eventId": "5097856"}  # Hardcoded eventID to test the event page
+user = {"username": "ageldartp"}
+sga = Seat_Geek_Api()
+dbc = DBController
 
 
 class FlaskTestCase(unittest.TestCase):
@@ -13,26 +20,29 @@ class FlaskTestCase(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-    # Tests the home page endpoint
-    def test_home(self):
-        response = self.app.get(BASE_URL + "/index")
-        data = json.loads(response.get_data())
-        self.assertIn("performers", str(data))  # Testing if data is correct
-        self.assertEqual(response.status_code, 200)  # Testing if endpoint is hitting
+    # Tests the home page endpoint function - if the response is return
+    @patch("Seat_Geek_API.requests.get")
+    def test_home(self, mock_get):
+        mock_get.return_value.status_code = 200
+        response = sga.getallEvents()
+        print("Get all event data: ", response)
+        self.assertIsNotNone(response)
 
-    # Tests the event page endpoint - data is returning and correct data is present
-    def test_eventData(self):
-        response = self.app.get(BASE_URL + "/event/" + eventId)
-        data = json.loads(response.get_data())
-        self.assertIn("performers_names", str(data))
-        self.assertEqual(response.status_code, 200)
+    # Tests the event page endpoint function - if the response is return
+    @patch("Seat_Geek_API.requests.get")
+    def test_eventData(self, mock_get):
+        mock_get.return_value.ok = True
+        response = sga.getEvent(eventId)
+        self.assertIsNotNone(response)
+        self.assertIn("status", response)
 
     # Tests the offered rides data endpoint - data is returning and correct data is present
-    def test_rideOffered(self):
-        response = self.app.get(BASE_URL + "/event/rides/" + eventId)
-        data = json.loads(response.get_data())
-        self.assertIn("RIDE_ID", str(data))
-        self.assertEqual(response.status_code, 200)
+    # @patch("Seat_Geek_API.requests.get")
+    # def test_rides(self):
+    #     mock_get.return_value.status_code = 200
+    #     dbc.getrides_username = MagicMock(return_value=ridesdata)
+    #     print("Get user data: ", response)
+    #     self.assertIsNotNone(response)
 
     # Tests the insert request in database endpoint - data is getting saved
     def test_saveRequest(self):
@@ -40,8 +50,8 @@ class FlaskTestCase(unittest.TestCase):
         Testdata = {
             "rideId": "125",
             "eventId": "1",
-            "userId": "aoheffernan3",
-            "status": "pending",
+            "userId": "testuser",
+            "status": "testpending",
         }
         response = self.app.post(
             BASE_URL + "/saveRequest",
