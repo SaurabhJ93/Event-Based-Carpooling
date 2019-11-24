9682867d1228891d73699be732036ef62f019186
kinet97 <neeleshmungolii@gmail.com>
2019-11-24 18:43:19 -0500

    Fixed the UserIcon
    fixed the user icons. Displays user according to its first_name

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
@@ -6,11 +6,16 @@ import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import icon from "../../assests/icon.png";
 import Signup from "../SignupModal";
+import Avatar ,{ ConfigProvider } from 'react-avatar';
+import jwt_decode from 'jwt-decode'
+
 
 const imgIcon = {
   width: "45px",
   marginRight: "10px"
 };
+
+
 
 const NavBar = props => {
 
@@ -28,10 +33,31 @@ const NavBar = props => {
     sessionStorage.clear(); //for sessionStorage
   };
 
+  const token = () => localStorage.usertoken;
+  const decoded = () => {jwt_decode(token)
+    this.setState({
+      first_name: decoded.identity.first_name,
+      last_name: decoded.identity.last_name,
+      email: decoded.identity.email
+    })  
+    
+  }
+  
+
+
+  
+  
+  
   return (
+
+
     <Navbar bg="dark" expand="lg" variant="dark">
+      
       <Navbar.Brand href="/" className="ml-lg-5 w-25">
+      
+
         <img src={icon} style={imgIcon} alt="Site logo" />
+        
         Event Based Carpool
       </Navbar.Brand>
 
@@ -47,17 +73,36 @@ const NavBar = props => {
       { localStorage.usertoken ? 
         [
         <Nav.Link href="/" onClick={handleLogout} >Logout</Nav.Link>, 
-        <Nav.Link href="/user" >Profile</Nav.Link>
+        <Nav.Link href="/user">
+        
+        
+        
+        
+
+        <Avatar name= {jwt_decode(localStorage.usertoken).identity.first_name} round={true} size={40}>
+        </Avatar>
+        </Nav.Link>
+        //<Nav.Link href="/user" >Profile</Nav.Link>
         ]  :  [
         <Nav.Link href="/Login" >Login </Nav.Link>, 
         <Button variant="outline-light" onClick={handleShow}>
         Signup
         </Button>, 
         <Signup show={showSignup} onSubmit = {handleSubmit}/>
-        ]
+
+        
+        
+        
+      ]
       }
+      
+      
     </Navbar>
-  );
-};
+
+
+  )
+    
+}
+
 
 export default NavBar;
--- client/src/pages/User/index.js
+++ client/src/pages/User/index.js
@@ -4,6 +4,11 @@ import "../../assests/styles/userStyle.css";
 import jwt_decode from 'jwt-decode'
 import background from "../../assests/background-event.jpg";
 import styled from "styled-components";
+import Avatar ,{ ConfigProvider } from 'react-avatar';
+
+
+
+
 
 const Styles = styled.div`
  .jumbo{
@@ -40,83 +45,7 @@ const Styles = styled.div`
    list-style-type: none;
   }`;
 
-// const users = {
-//     name: "Infinitys end",
-//     address: "9201 University City Blvd",
-//     contact: "8457098904",
-//     email: "infinitysend@ymail.com",
-// };
 
-// const offeredRide = [];
-// let i = 0;
-// for (i = 1; i <= 10; i++) {
-//     offeredRide.push(`Offer ${i}`);
-// }
-
-// const requestedRide = [];
-// let j = 0;
-// for (j = 1; j <= 5; j++) {
-//     requestedRide.push(`Request ${j}`);
-// }
-
-// const User = () => {
-//     return (
-//         <div className="container-fluid">
-//             <div className="row">
-//                 <div className="col-sm-4 text-right" >
-//                     <img class="masthead-avatar mb-5" src={userImg} alt="" />
-//                 </div>
-//                 <div className="user-data col-sm-4">
-//                     <p class="masthead-name text-uppercase mb-2 mt-4" >{users.name}</p>
-//                     <p class="mb-2">{users.contact}</p>
-//                     <p class="mb-2">{users.email}</p>
-//                     <p class="mb-2">{users.address}</p>
-//                 </div>
-//                 {/* <div className="col-sm-4 justify-content-center align-self-center">
-//                     <button type="button" class="btn btn-dark btn-lg">
-//                         Edit Details
-//                      </button>
-//                 </div> */}
-//             {/* </div> */}
-//             {/* <div className="row mt-5 w-75 mx-auto">
-//                 <div class="col-lg-6 lg-offset-6">
-//                     <div class="user-offered-rides">
-//                         <p className="heading-req">Your Offered Rides</p>
-//                         <ul class="list-group">
-//                             {offeredRide.map(ride => (
-//                                 <li class="list-group-item bg-light">
-//                                     <span class="float-left p-rider">{ride}</span>
-//                                     <button type="button" class="btn btn-dark btn-sm float-right font-weight-bold">
-//                                         Decline
-//                                         </button>&nbsp;
-//                                         <button type="button" class="btn btn-dark btn-sm float-right mr-2 font-weight-bold">
-//                                         Accept
-//                                         </button>
-//                                 </li>
-//                             ))}
-//                         </ul>
-//                     </div>
-//                 </div> */}
-//                 {/* <div class="col-lg-6 lg-offset-6">
-//                     <div class="user-requested-rides">
-//                         <p className="heading-req">Your Requested Rides</p>
-//                         <ul class="list-group">
-//                             {requestedRide.map(ride => (
-//                                 <li class="list-group-item bg-light">
-//                                     <p class="p-rider">{ride}</p>
-//                                     <p class=" p-rider">Name, address and other details</p>
-//                                     <span class="text-warning float-right">Pending</span>
-//                                     <span class="text-danger float-right mr-2">Declined</span>
-//                                     <span class="text-success float-right mr-2">Accepted</span>
-//                                 </li>
-//                             ))}
-//                         </ul>
-//                     </div>
-//                 </div> */}
-//             </div>
-//         </div>
-//     );
-// };
 class User extends Component {
     constructor() {
       super()
@@ -141,10 +70,12 @@ class User extends Component {
     render() {
       return (
         <Styles>
+
         <div className="container">
           <div className="jumbotron mt-1">
             <div className="col-sm-8 mx-auto">
               <h1 className="text-center">User Profile</h1>
+              
             </div>
             <table className="table col-md-6 mx-auto">
               <tbody>
@@ -164,6 +95,7 @@ class User extends Component {
             </table>
           </div>
         </div>
+
         </Styles>
       )
     }
