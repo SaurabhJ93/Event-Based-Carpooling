819f9ae1e4ce36f7192def98f05ab19ae3c06a5f
AnushreeSrivastava <anu1106.shree@gmail.com>
2019-11-24 21:57:21 -0500

    user page changes

--- client/src/pages/User/index.js
+++ client/src/pages/User/index.js
@@ -4,7 +4,7 @@ import "../../assests/styles/userStyle.css";
 import jwt_decode from 'jwt-decode'
 import background from "../../assests/background-event.jpg";
 import styled from "styled-components";
-import Avatar ,{ ConfigProvider } from 'react-avatar';
+import Avatar, { ConfigProvider } from 'react-avatar';
 import { useFetch, SubmitRequest } from "./Backendhooks"; //to handle fetch data request from flask
 
 
@@ -47,139 +47,143 @@ const Styles = styled.div`
   }`;
 
 
-class User extends Component {
-    constructor() {
-      super()
-      this.state = {
-        first_name: '',
-        last_name: '',
-        email: '',
-        errors: {}
-      }
-    }
-  
-    componentDidMount() {
-      const token = localStorage.usertoken
-      const decoded = jwt_decode(token)
-      this.setState({
-        first_name: decoded.identity.first_name,
-        last_name: decoded.identity.last_name,
-        email: decoded.identity.email
-      })
-    }
-  
-    render() {
-      return (
-        <Styles>
+// class User extends Component {
+//     constructor() {
+//       super()
+//       this.state = {
+//         first_name: '',
+//         last_name: '',
+//         email: '',
+//         errors: {}
+//       }
+//     }
 
-        <div className="container">
-          <div className="jumbotron mt-1">
-            <div className="col-sm-8 mx-auto">
-              <h1 className="text-center">User Profile</h1>
-              
-            </div>
-            <table className="table col-md-6 mx-auto">
-              <tbody>
-                <tr>
-                  <td>Fist Name</td>
-                  <td>{this.state.first_name}</td>
-                </tr>
-                <tr>
-                  <td>Last Name</td>
-                  <td>{this.state.last_name}</td>
-                </tr>
-                <tr>
-                  <td>Email</td>
-                  <td>{this.state.email}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+//     componentDidMount() {
+//       const token = localStorage.usertoken
+//       const decoded = jwt_decode(token)
+//       this.setState({
+//         first_name: decoded.identity.first_name,
+//         last_name: decoded.identity.last_name,
+//         email: decoded.identity.email
+//       })
+//     }
 
-        </Styles>
-      )
-    }
-  }
+//     render() {
+//       return (
+//         <Styles>
 
-															
-									   
+//         <div className="container">
+//           <div className="jumbotron mt-1">
+//             <div className="col-sm-8 mx-auto">
+//               <h1 className="text-center">User Profile</h1>
+
+//             </div>
+//             <table className="table col-md-6 mx-auto">
+//               <tbody>
+//                 <tr>
+//                   <td>Fist Name</td>
+//                   <td>{this.state.first_name}</td>
+//                 </tr>
+//                 <tr>
+//                   <td>Last Name</td>
+//                   <td>{this.state.last_name}</td>
+//                 </tr>
+//                 <tr>
+//                   <td>Email</td>
+//                   <td>{this.state.email}</td>
+//                 </tr>
+//               </tbody>
+//             </table>
+//           </div>
+//         </div>
+
+//         </Styles>
+//       )
+//     }
+//   }
+
 
 const User = () => {
-    const [user, offered_rides, requested_rides, hasErrors] = useFetch(); // to call flask/backend server		
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-sm-4 text-right" >
-                    <img className="masthead-avatar mb-5" src={userImg} alt="profile pic" />
-                </div>   
-                <div className="user-data col-sm-4">
-                    <p className="masthead-name text-uppercase mb-2 mt-4"> {user.name +" ( "+user.username+" ) "} </p>
-                    <p className="mb-2" > {user.contact} </p>
-                    <p className="mb-2" > {user.email} </p>
-											 
-                </div>
-                {/* <div className="col-sm-4 justify-content-center align-self-center">
+  const [user, offered_rides, requested_rides, hasErrors] = useFetch(); // to call flask/backend server		
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-4 text-right" >
+          <img className="masthead-avatar mb-5" src={userImg} alt="profile pic" />
+        </div>
+        <div className="user-data col-sm-4">
+          <p className="masthead-name text-uppercase mb-2 mt-4"> {user.name + " ( " + user.username + " ) "} </p>
+          <p className="mb-2" > {user.contact} </p>
+          <p className="mb-2" > {user.email} </p>
+
+        </div>
+        {/* <div className="col-sm-4 justify-content-center align-self-center">
                     <button type="button" className="btn btn-dark btn-lg">
                         Edit Details
                      </button>
                 </div> */}
-            </div>
-            <div className="row mt-5 w-75 mx-auto">
-                <div className="col-lg-6 lg-offset-6">
-                    <div className="user-offered-rides">
-                        <p className="heading-req">Your Offered Rides</p>
-                        <ul className="list-group">
-                            {offered_rides.length> 0 ?
-                                <>{offered_rides.map(ride => (
-                                <li className="list-group-item bg-light" key={ride.request_id}>
-                                    <p className="float-left p-rider">Name: {ride.name}</p>
-                                    <p className="float-left p-rider">Contact: {ride.contact_no}</p>
-                                    <p className="float-left p-rider">Email: {ride.email}</p>
-                                    { ride.status === 'pending' ? <>
-                                    <button onClick={async () => await SubmitRequest(ride.request_id, "declined")} type="button" className="btn btn-dark btn-sm float-right font-weight-bold" > 
-                                        Decline 
+      </div>
+      <div className="row mt-5 w-75 mx-auto">
+        <div className="col-lg-6 lg-offset-6">
+          <div className="user-offered-rides">
+            <p className="heading-req">Requests Received</p>
+            <ul className="list-group">
+              {offered_rides.length > 0 ?
+                <>{offered_rides.map(ride => (
+                  <li className="list-group-item bg-light" key={ride.request_id}>
+                    <p className="p-rider">Name: {ride.name}</p>
+                    <p className="p-rider">Contact: {ride.contact_no}</p>
+                    <p className="p-rider">Email: {ride.email}</p>
+                    <p className="p-rider">Event Name: {ride.event_name}</p>
+                    <p className="p-rider">Date/Time: {ride.date_time}</p>
+                    <p className="p-rider">Location: {ride.address}</p>
+                    {ride.status === 'pending' ? <>
+                      <button onClick={async () => await SubmitRequest(ride.request_id, "declined")} type="button" className="btn btn-dark btn-sm float-right font-weight-bold" >
+                        Decline
                                     </button>&nbsp;
                                     <button onClick={async () => await SubmitRequest(ride.request_id, "accepted")} type="button" className="btn btn-dark btn-sm float-right mr-2 font-weight-bold"  >
-                                        Accept
+                        Accept
                                     </button>
-                                    </>
-                                    : 
-                                    <button type="button" className="btn btn-dark btn-sm float-right mr-2 font-weight-bold" disabled>
-                                        Already Accepted!
+                    </>
+                      :
+                      <button type="button" className="btn btn-dark btn-sm float-right mr-2 font-weight-bold" disabled>
+                        Already Accepted!
                                     </button>
-                                    }
-                                </li>
-                            ))}
-                            </> : <> <p className="float-left p-rider">No Rides Offered....</p> </>}
-                        </ul>
-                    </div>
-                </div> 
-                <div className="col-lg-6 lg-offset-6">
-                    <div className="user-requested-rides">
-                        <p className="heading-req">Your Requested Rides</p>
-                        <ul className="list-group">
-                            { requested_rides.length > 0 ?
-                            <>
-                            {requested_rides.map(ride => (
-                                <li className="list-group-item bg-light" key={ride.request_id}>
-                                    <p className=" p-rider">Host Name: {ride.name}</p>
-                                    { ride.status==="pending" ? 
-                                    <span className="text-warning float-right">Pending</span> 
-                                    : ride.status==="accepted" ?
-                                    <span className="text-danger float-right mr-2">Accepted</span>
-                                    :
-                                    <span className="text-success float-right mr-2">Declined</span>
-                                    }
-                                </li>
-                            ))}
-                            </> : <> <p className="float-left p-rider">No Rides Requested....</p> </>}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>  
-    )
+                    }
+                  </li>
+                ))}
+                </> : <> <p className="float-left p-rider">No Rides Offered....</p> </>}
+            </ul>
+          </div>
+        </div>
+        <div className="col-lg-6 lg-offset-6">
+          <div className="user-requested-rides">
+            <p className="heading-req">Requests made</p>
+            <ul className="list-group">
+              {requested_rides.length > 0 ?
+                <>
+                  {requested_rides.map(ride => (
+                    <li className="list-group-item bg-light" key={ride.request_id}>
+                      <p className=" p-rider">Host Name: {ride.name}</p>
+                      <p className="p-rider">Event Name: {ride.event_name}</p>
+                      <p className="p-rider">Date/Time: {ride.date_time}</p>
+                      <p className="p-rider">Location: {ride.address}</p>
+                      {ride.status === "pending" ?
+                        <span className="text-warning float-right">Pending</span>
+                        : ride.status === "accepted" ?
+                          <span className="text-danger float-right mr-2">Accepted</span>
+                          :
+                          <span className="text-success float-right mr-2">Declined</span>
+                      }
+                    </li>
+                  ))}
+                </> : <> <p className="float-left p-rider">No Rides Requested....</p> </>}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default User;
