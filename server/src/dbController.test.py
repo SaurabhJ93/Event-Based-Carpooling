f8185fe1aeb988afb0ef0e37fbee317abfe6a82d
Saurabh Jaiswal <saurabhjaiswal@Image-5-3-19.local>
2019-11-26 11:38:40 -0500

    Added test cases for enterUser db

--- server/src/dbController.test.py
+++ server/src/dbController.test.py
@@ -1,32 +1,3 @@
-# import unittest
-# from unittest.mock import patch
-# import sys
-# from flask import Flask
-# from flask_mysqldb import MySQL
-# from Database_Layer.dbController import DBController
-# import DB_config
-
-
-# app = Flask('__main__')
-# app.config['TESTING'] = True
-# app.config["MYSQL_HOST"] = DB_config.MYSQL_HOST
-# app.config["MYSQL_USER"] = DB_config.MYSQL_USER
-# app.config["MYSQL_PASSWORD"] = DB_config.MYSQL_PASSWORD
-# app.config["MYSQL_DB"] = DB_config.MYSQL_DB_TEST
-# app.config["CORS_HEADERS"] = DB_config.CORS_HEADERS
-# app.config["MYSQL_CURSORCLASS"] = DB_config.MYSQL_CURSORCLASS
-    #  @patch('Seat_Geek_API.requests.get')
-    # def test_home(self, mock_get):
-    #     raise_for_status = Mock()        
-    #     mock_get.return_value.raise_for_status= raise_for_status
-    #     mock_get.return_value = self.sampleResponse
-    #     response = self.app.get(BASE_URL + "/index")
-    #     data = json.loads(response.get_data())
-    #     print('checking response code. is: ', response.status_code)
-    #     mock_get.assert_called_once()
-    #     self.assertEqual(response.status_code, 200)  # Testing if endpoint is hitting
-
-
 import MySQLdb
 import unittest
 import DB_config
@@ -44,10 +15,15 @@ class TestDBController(unittest.TestCase):
         self.cur = self.db.cursor()
         self.controller = DBController(self.cur, self.db)
         self.username = 'rakgunti26'
-        self.ride_id = 181
+        self.ride_id = 182
         self.event_id = '5096273'
-        self.request_id = 52
+        self.request_id = 51
         self.status = 'accepted'
+        self.firstName = 'Wall'
+        self.lastName = 'Street'
+        self.email='fak1e@gmail.com'
+        self.phoneNumber = '123-456-7890'
+        self.password = "welcome123"
 
     def tearDown(self):
         self.cur.close()
@@ -102,7 +78,74 @@ class TestDBController(unittest.TestCase):
         self.cur.execute(""" DELETE FROM RIDES_REQUESTED WHERE REQUEST_ID=%s AND STATUS=%s""", (self.request_id, self.status), )
         self.db.commit()
 
+    def test_enterUserSuccess(self):
+        data = {
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "password": self.password,
+            "phoneNumber": self.phoneNumber,
+            "email": self.email
+        }
+        print('data', data)
+        response = self.controller.enterUser(data)
+        print('response', response)
+        self.assertEqual(response, "Success")
+        self.cur.execute(""" SELECT * FROM USER WHERE FIRST_NAME=%s AND LAST_NAME=%s""", (self.firstName, self.lastName))
+        result = self.cur.fetchall()[0]
+        print('result', result)
+        self.assertEqual(result[2], self.firstName)
+        self.assertEqual(result[3], self.lastName)
+        self.assertEqual(result[4], self.phoneNumber)
+        self.assertEqual(result[5], self.email)
+
+    def test_enterUserFailure(self):
+        data = {
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "password": self.password,
+            "phoneNumber": self.phoneNumber,
+            "email": self.email
+        }
+        print('data', data)
+        response = self.controller.enterUser(data)
+        print('response', response)        
+        self.assertEqual(response, "Email already present. Try a new email-id")
+        #To delete inserted data
+        self.cur.execute(""" DELETE FROM USER WHERE FIRST_NAME=%s AND LAST_NAME=%s""", (self.firstName, self.lastName))
+        self.db.commit()
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+# import unittest
+# from unittest.mock import patch
+# import sys
+# from flask import Flask
+# from flask_mysqldb import MySQL
+# from Database_Layer.dbController import DBController
+# import DB_config
+
+
+# app = Flask('__main__')
+# app.config['TESTING'] = True
+# app.config["MYSQL_HOST"] = DB_config.MYSQL_HOST
+# app.config["MYSQL_USER"] = DB_config.MYSQL_USER
+# app.config["MYSQL_PASSWORD"] = DB_config.MYSQL_PASSWORD
+# app.config["MYSQL_DB"] = DB_config.MYSQL_DB_TEST
+# app.config["CORS_HEADERS"] = DB_config.CORS_HEADERS
+# app.config["MYSQL_CURSORCLASS"] = DB_config.MYSQL_CURSORCLASS
+    #  @patch('Seat_Geek_API.requests.get')
+    # def test_home(self, mock_get):
+    #     raise_for_status = Mock()        
+    #     mock_get.return_value.raise_for_status= raise_for_status
+    #     mock_get.return_value = self.sampleResponse
+    #     response = self.app.get(BASE_URL + "/index")
+    #     data = json.loads(response.get_data())
+    #     print('checking response code. is: ', response.status_code)
+    #     mock_get.assert_called_once()
+    #     self.assertEqual(response.status_code, 200)  # Testing if endpoint is hitting
