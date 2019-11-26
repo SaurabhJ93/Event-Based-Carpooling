1b066a0a09e9eb3d12827ace68b09b119ba29b73
rgunti1 <46797008+rgunti1@users.noreply.github.com>
2019-11-26 01:23:34 -0500

    Added one more test function
    In dbController.test.py file, added a function which will validate Update request methods in DBcontroller,py

--- server/src/dbController.test.py
+++ server/src/dbController.test.py
@@ -15,11 +15,22 @@
 # app.config["MYSQL_DB"] = DB_config.MYSQL_DB_TEST
 # app.config["CORS_HEADERS"] = DB_config.CORS_HEADERS
 # app.config["MYSQL_CURSORCLASS"] = DB_config.MYSQL_CURSORCLASS
+    #  @patch('Seat_Geek_API.requests.get')
+    # def test_home(self, mock_get):
+    #     raise_for_status = Mock()        
+    #     mock_get.return_value.raise_for_status= raise_for_status
+    #     mock_get.return_value = self.sampleResponse
+    #     response = self.app.get(BASE_URL + "/index")
+    #     data = json.loads(response.get_data())
+    #     print('checking response code. is: ', response.status_code)
+    #     mock_get.assert_called_once()
+    #     self.assertEqual(response.status_code, 200)  # Testing if endpoint is hitting
 
 
 import MySQLdb
 import unittest
 import DB_config
+from unittest.mock import patch
 from Database_Layer.dbController import DBController
 
 class TestDBController(unittest.TestCase):
@@ -35,14 +46,15 @@ class TestDBController(unittest.TestCase):
         self.username = 'rakgunti26'
         self.ride_id = 181
         self.event_id = '5096273'
+        self.request_id = 52
         self.status = 'accepted'
 
     def tearDown(self):
+        self.cur.close()
         self.db.close()
     
-
-    def test_saveRequest(self):
-        self.controller.saveRequest(self.ride_id, self.event_id, self.username, self.status)
+    def test_getUserData(self):
+        result =self.controller.getUser(self.username)
         self.cur.execute(""" SELECT RIDE_ID, EVENT_ID, USERNAME, STATUS FROM RIDES_REQUESTED WHERE RIDE_ID=%s AND EVENT_ID=%s AND USERNAME=%s""", (self.ride_id, self.event_id, self.username))
         result = self.cur.fetchall()[0]
         self.assertEqual(result[0], self.ride_id)
@@ -51,18 +63,45 @@ class TestDBController(unittest.TestCase):
         self.assertEqual(result[3], self.status)
         self.cur.execute(""" DELETE FROM RIDES_REQUESTED WHERE RIDE_ID=%s AND EVENT_ID=%s AND USERNAME=%s""", (self.ride_id, self.event_id, self.username), )
         self.db.commit()
+    
+    def test_saveRequest(self):
+        self.controller.saveRequest(self.ride_id, self.event_id, self.username, self.status)
+        self.cur.execute(""" SELECT RIDE_ID, EVENT_ID, USERNAME, STATUS FROM RIDES_REQUESTED WHERE RIDE_ID=%s AND EVENT_ID=%s AND USERNAME=%s""", (self.ride_id, self.event_id, self.username))
+        result = self.cur.fetchall()[0]
+        self.assertEqual(result[0], self.ride_id)
+        self.assertEqual(result[1], self.event_id)
+        self.assertEqual(result[2], self.username)
+        self.assertEqual(result[3], self.status)
+        #To check count of the table given details in where clause
+        self.cur.execute(""" SELECT COUNT(*) FROM RIDES_REQUESTED WHERE RIDE_ID=%s AND EVENT_ID=%s AND USERNAME=%s""", (self.ride_id, self.event_id, self.username))
+        result = self.cur.fetchall()[0]
+        self.assertEqual(result[0],1)
+        #To delete inserted data
+        self.cur.execute(""" DELETE FROM RIDES_REQUESTED WHERE RIDE_ID=%s AND EVENT_ID=%s AND USERNAME=%s""", (self.ride_id, self.event_id, self.username), )
+        self.db.commit()
 
+    def test_updateRequest(self):
+        self.cur.execute(""" SELECT STATUS FROM RIDES_REQUESTED WHERE REQUEST_ID=%s""", (self.request_id))
+        before_status = self.cur.fetchall()[0][0]
+        self.assertEqual(self.status, before_status)
+        
+        self.controller.saveRequest(self.request_id, self.status)
+        
+        self.cur.execute(""" SELECT REQUEST_ID, STATUS FROM RIDES_REQUESTED WHERE REQUEST_ID=%s AND STATUS=%s""", (self.request_id, self.status))
+        result = self.cur.fetchall()[0]
+        self.assertEqual(result[0], self.request_id)
+        self.assertEqual(result[1], self.status)
+        self.assertEqual(before_status, result)
 
-    #  @patch('Seat_Geek_API.requests.get')
-    # def test_home(self, mock_get):
-    #     raise_for_status = Mock()        
-    #     mock_get.return_value.raise_for_status= raise_for_status
-    #     mock_get.return_value = self.sampleResponse
-    #     response = self.app.get(BASE_URL + "/index")
-    #     data = json.loads(response.get_data())
-    #     print('checking response code. is: ', response.status_code)
-    #     mock_get.assert_called_once()
-    #     self.assertEqual(response.status_code, 200)  # Testing if endpoint is hitting
+        #To check count of the table given details in where clause
+        self.cur.execute(""" SELECT COUNT(*) FROM RIDES_REQUESTED WHERE REQUEST_ID=%s AND STATUS=%s """, (self.request_id, self.status))
+        result = self.cur.fetchall()[0]
+        self.assertEqual(result[0],1)
+        
+        #To delete inserted data
+        self.cur.execute(""" DELETE FROM RIDES_REQUESTED WHERE REQUEST_ID=%s AND STATUS=%s""", (self.request_id, self.status), )
+        self.db.commit()
+
 
 
 if __name__ == '__main__':
